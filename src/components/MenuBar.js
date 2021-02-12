f432fb1dfe7c0c3084e9cd2c269af31c035fac48
Jordan Leslie <this.codedev@gmail.com>
2021-02-12 10:02:49 -0600

    Hide toolbar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,33 +3,33 @@ import MenuIcon from '@material-ui/icons/Menu'
 import { AppBar, Toolbar, IconButton, Typography, Button, makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      borderRadius: '10px',
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+    borderRadius: '10px',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
 const MenuBar = () => {
-    const classes = useStyles()
+  const classes = useStyles()
 
-    return (
+  return (
     <AppBar position="static" color="default" maxWidth="lg" className={classes.root}>
-      <Toolbar>
+      {/* <Toolbar>
         <Typography variant="h6" className={classes.title}>
           About
         </Typography>
         <Typography variant="h6" className={classes.title}>
           <a href="#quiz">Take The Quiz</a>
         </Typography>
-      </Toolbar>
+      </Toolbar> */}
     </AppBar>
-    )
+  )
 }
 
 export { MenuBar }
